76a389c2325daebc69d3c8b3f12405f965931ef6
Erick Javier Cruz <erickcroz2013@gmail.com>
2022-07-14 19:05:06 -0600

    add button to go manage products

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',function(req,res,next){
-  res.send("Welcome to Manage Supermarket");
+  res.render("index",{title:"Begin"});
 });
 
 module.exports = router;
